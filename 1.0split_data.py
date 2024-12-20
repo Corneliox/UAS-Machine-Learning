d4fce3010ccfb0d5424764a7b9dbd9a73f574640
Cornelio <cornelioabdimash@gmail.com>
2024-12-20 20:36:26 +0700

    Labels from split fox

--- 1.0split_data.py
+++ 1.0split_data.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 
 # Paths
 images_dir = 'images'
-labels_dir = 'labels'
+labels_dir = 'label'
 
 train_dir = 'train'
 val_dir = 'val'
@@ -13,7 +13,7 @@ test_dir = 'test'
 # Output directories
 for split in [train_dir, val_dir, test_dir]:
     os.makedirs(f'{split}/images', exist_ok=True)
-    os.makedirs(f'{split}/labels', exist_ok=True)
+    os.makedirs(f'{split}/label', exist_ok=True)
 
 # All image files
 image_files = [f for f in os.listdir(images_dir) if f.endswith('.jpg')]
@@ -28,7 +28,7 @@ def copy_files(file_list, split_dir):
         
         label_file = os.path.splitext(file)[0] + '.txt'
         src_label_path = os.path.join(labels_dir, label_file)
-        dst_label_path = os.path.join(split_dir, 'labels', label_file)
+        dst_label_path = os.path.join(split_dir, 'label', label_file)
 
         if os.path.exists(src_label_path):
             shutil.copy(src_label_path, dst_label_path)
